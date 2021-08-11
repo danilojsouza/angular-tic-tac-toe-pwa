f35406876c966be1c7cf6a2f73e893030afd4fc1
Danilo Souza <danilosouza.bm@gmail.com>
2021-08-11 12:14:56 -0300

    Beautifying the game

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,7 +2,10 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `<button >{{value}}</button>`,
+  template: `
+  <button nbButton *ngIf="!value">{{value}}</button>
+  <button nbButton hero status="success" *ngIf="value == 'X'">{{value}}</button>
+  <button nbButton hero status="info" *ngIf="value == 'O'">{{value}}</button>`,
   styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
