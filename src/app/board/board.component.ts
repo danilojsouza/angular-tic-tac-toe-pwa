7509339cc5165fb2c7eb616c5957246df6bd7719
Danilo Souza <danilosouza.bm@gmail.com>
2021-08-11 11:22:27 -0300

    Add app, board and square components

---
+++ src/app/board/board.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-board',
+  template: `
+    <p>
+      board works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class BoardComponent implements OnInit {
+  ngOnInit(): void {
+  }
+  
+}
