c2e9609ccfe372fee3b2b1ad37481eb311f6f5be
Danilo Souza <danilosouza.bm@gmail.com>
2021-08-11 11:51:34 -0300

    Implementing the game

--- src/app/square/square.component.ts
+++ src/app/square/square.component.ts
@@ -2,13 +2,11 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square',
-  template: `
-  `,
-  styles: [
-  ]
+  template: `<button >{{value}}</button>`,
+  styleUrls: ['./square.component.scss']
 })
 export class SquareComponent {
 
-  
+  @Input() value!: 'X' | 'O';
 
 }
